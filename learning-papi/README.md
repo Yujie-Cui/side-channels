bb6d9b7459f02b58319fd0f12573b0829ca8555e
atskae <atskae@gmail.com>
2019-04-28 11:26:26 -0400

    Added a learning-papi directory and notes

---
+++ learning-papi/README.md
@@ -0,0 +1,22 @@
+# Learning How to use PAPI
+
+## Install PAPI
+[Downloading and Installing PAPI](http://icl.utk.edu/papi/software/)
+1. Clone the repository
+```
+git clone https://bitbucket.org/icl/papi.git
+```
+
+2. Go to `papi/src` directory
+```
+cd papi/src
+```
+
+3. Run configuration script and build PAPI
+```
+./configure
+make
+```
+
+## Resources
+[pAPI User's Guide](http://icl.cs.utk.edu/projects/papi/files/documentation/PAPI_USER_GUIDE.htm#INTRODUCTION_TO_PAPI)
--- spectre/prefetch/spectre.c
+++ spectre/prefetch/spectre.c
@@ -92,6 +92,9 @@ int main(int argc, char* argv[]) {
 			buffer_item = access_buffer(secret_idx);
 			probe_array[buffer_item * PAGE_SIZE + DELTA] = 1;
 			_mm_prefetch(&probe_array[(buffer_item+20)*PAGE_SIZE + DELTA], 2);
+            _mm_prefetch(&probe_array[(buffer_item+43)*PAGE_SIZE + DELTA], 2);
+
+            //_mm_prefetch(&probe_array[buffer_item*(PAGE_SIZE+20) + DELTA], 2);
 
 			// measure the clflush() instruction for each element
 			for(int i=0; i<ARRAY_SIZE; i++) {
